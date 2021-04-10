b8fecfaf0ea7a1f53a8409f64e05b6d787bd8112
Nikita <bat17v@gmail.com>
2021-04-10 21:40:38 +0300

    Вынес transform_img в отдельную функцию и добавил параметр square.

--- utils.py
+++ utils.py
@@ -16,13 +16,22 @@ def resource_path(relative_path: str):
 def load_img(file_name: str, size: tuple = (None, None), flip: tuple = (False, False)):
     img = pygame.image.load(resource_path(file_name))
 
+    return transform_img(img, size, flip)
+
+
+def transform_img(img, size: tuple = (None, None), flip: tuple = (False, False), square=None):
+
     if type(size[0]) == int and type(size[1]) == int:
         img = pygame.transform.scale(img, size)
-
     if type(size[0]) == float and type(size[1]) == float:
         x, y = img.get_size()
         img = pygame.transform.scale(img, (int(size[0] * x), int(size[1] * y)))
 
+    if type(square) == int:
+        m = max(size)
+        s0 = size[0] / (m / square)
+        s1 = size[1] / (m / square)
+        img = pygame.transform.scale(img, (int(s0), s1))
     img = pygame.transform.flip(img, flip[0], flip[1])
     return img
 
