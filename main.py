841c21b09cfd4ac07780862089990d246c344257
Nikita <bat17v@gmail.com>
2021-04-17 10:46:33 +0300

    Реализовал создание действия у кнопки.
    Добавил кнопку паузы(и функцию для нее).
    Теперь обновление и отрисовка всех объектов(включая фон) делается только если идет игра.

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ from tower import *
 from menu import *
 
 
+def pause_game():
+    global play
+    play = not play
+
+
 pygame.init()
 screen = pygame.display.set_mode((1000, 588 + int(192 * 0.50556)))  # TODO: увеличить высоту для панели
+play = True
 screens = [
     load_img('f/screens/screen0.png', size=(1000, 588))
 ]
@@ -14,26 +20,29 @@ world = [
     StoneTower(200, 200, stone_imgs['stone'], tower_imgs['stone'])
 ]
 t = 0
-# ggdgdgdh
+
 buttons = [
-    # TODO: нужно создать кнопки и панель соответствующих классов
+    Button(881, 560, menu_imgs['buttons']['action']['pause'], pause_game)
 ]
-p = Panel(0, 588, menu_imgs['panel'])
+p = Panel(0, 588, menu_imgs['panel'], buttons)
 while True:
-    screen.blit(screens[0], (0, 0))
-    pygame.time.delay(10)
-    t += 1
-    if t % 150 == 0:
-        world.append(Enemy(open_path('f/levels/paths/level_1.2.txt'), 'run', enemy_imgs['scorpion']))
+    if play:
+        screen.blit(screens[0], (0, 0))
+        pygame.time.delay(10)
+        t += 1
+        if t % 150 == 0:
+            world.append(Enemy(open_path('f/levels/paths/level_1.2.txt'), 'run', enemy_imgs['scorpion']))
+
+        for elem in world:
+            elem.update()
+            elem.draw(screen)
+
+        p.draw(screen)
 
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             exit()
-
-    for elem in world:
-        elem.update()
-        elem.draw(screen)
-
-    p.draw(screen)
+        elif e.type == pygame.MOUSEBUTTONUP:
+            p.try_push(*pygame.mouse.get_pos())
 
     pygame.display.flip()
--- menu.py
+++ menu.py
@@ -4,45 +4,45 @@ from utils import *
 
 menu_imgs = {
     'buttons': {
-        'touch': [load_img(f'f/icons/ico_{i + 1}.png') for i in range(25)],
-        'cost': load_img('f/icons/ico_cost.png'),
+        'ico': [load_img(f'f/buttons/ico_{i + 1}.png', size=(0.50556, 0.50556)) for i in range(23)],
+        'action': {action: load_img(f'f/buttons/actions/{action}.png', size=(0.50556, 0.50556)) for action in ('faster', 'pause')}
     },
-    'panel': load_img('f/icons/panel.png', size=(0.50556, 0.50556)),
+    'panel': load_img('f/buttons/panel.png', size=(0.50556, 0.50556)),
+    'cost': load_img('f/buttons/ico_cost.png', size=(0.50556, 0.50556)),
 }
 
 
 class Button:
-    def __init__(self, x: int, y: int, img: pygame.SurfaceType, cost: int = 0):
+    def __init__(self, x: int, y: int, img: pygame.SurfaceType, command=lambda: None, cost: int = 0):
         self.pos = x, y
         self.rect = pygame.Rect(x, y, *img.get_size())
         self.img = img
         self.cost = cost
+        self.command = command
 
     def draw(self, screen: pygame.SurfaceType):
         screen.blit(self.img, self.pos)
 
-    def action(self):
-        pass  # TODO: реализовать создание действия кнопки
-
-    def update(self, x: int, y: int):
+    def try_push(self, x: int, y: int):
         if self.rect.collidepoint(x, y):
-            self.action()
+            self.command()
 
 
 class Panel:
-    def __init__(self, x: int, y: int, img: pygame.SurfaceType, buttons: list = []):
+    def __init__(self, x: int, y: int, img: pygame.SurfaceType, buttons=None):
+        if buttons is None:
+            buttons = []
         self.pos = x, y
         self.rect = pygame.Rect(x, y, *img.get_size())
         self.img = img
         self.buttons = buttons
 
     def draw(self, screen: pygame.SurfaceType):
-        print('drawing Panel:', self.rect)
-        pygame.draw.rect(screen, (255, 0, 0), self.rect)
+        # pygame.draw.rect(screen, (255, 0, 0), self.rect)
         screen.blit(self.img, self.pos)
         for button in self.buttons:
             button.draw(screen)
 
-    def update(self, x: int, y: int):
+    def try_push(self, x: int, y: int):
         for button in self.buttons:
-            button.update(x, y)
+            button.try_push(x, y)
